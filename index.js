7095d1e78bd619be0ac56e471950fbd027ae744e
Krystyna Szurmańska <kszurmanska@gmail.com>
2018-06-25 22:21:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
+var prefix = "https://cors-anywhere.herokuapp.com/";
+
 var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
 function getQuote() {
-    fetch(quoteUrl, { cache: "no-store" })
+    fetch(prefix + quoteUrl, { cache: "no-store" })
         .then(function(resp) {
             return resp.json();
         })
         .then(createTweet);
 }
+
+
 
 function createTweet(input) {
     var data = input[0];
@@ -33,10 +37,3 @@ function createTweet(input) {
     }
 
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-    getQuote();
-    document.querySelector('.trigger').addEventListener('click', function() {
-        getQuote();
-    });
-});
